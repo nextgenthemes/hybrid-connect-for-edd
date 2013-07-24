ad4556c9ac4dacf4f455a2bc0e3f575ceed8628c
Thiago Senna <thiago@thremes.com.br>
2013-07-24 11:38:06 -0300

    loop-meta moved to templates

--- functions.php
+++ functions.php
@@ -31,42 +31,11 @@ function hcf_edd_has_loop_meta()
  */
 function hcf_edd_loop_meta($wrap = false)
 {
-    if (!hcf_edd_has_loop_meta()) {
+    if (!locate_template('loop-meta-downloads.php', true, false)) {
 
-        return;
+        // if no template found output the default one
+        require(HCF_EDD_TEMPLATES_DIR . 'loop-meta-downloads.php');
     }
-
-    echo '<div class="loop-meta">';
-    if ($wrap) echo apply_filters('hcf_edd_open_loop_meta_wrap', '<div class="wrap">');
-
-    if (is_tax('download_category') || is_tax('download_tag')) {
-        ?>
-
-        <h1 class="loop-title"><?php single_term_title(); ?></h1>
-
-        <div class="loop-description">
-            <?php echo term_description('', get_query_var('taxonomy')); ?>
-            <?php hcf_edd_download_menu(); ?>
-        </div><!-- .loop-description -->
-
-    <?php
-    } elseif (is_post_type_archive('download')) {
-        ?>
-
-        <?php $post_type = get_post_type_object(get_query_var('post_type')); ?>
-
-        <h1 class="loop-title"><?php post_type_archive_title(); ?></h1>
-
-        <div class="loop-description">
-            <?php if (!empty($post_type->description)) echo wpautop($post_type->description); ?>
-            <?php hcf_edd_download_menu(); ?>
-        </div><!-- .loop-description -->
-
-    <?php
-    }
-
-    if ($wrap) echo apply_filters('hcf_edd_close_loop_meta_wrap', '</div><!-- .wrap -->');
-    echo '</div><!-- .loop-meta -->';
 }
 
 /**
---
+++ templates/loop-meta-downloads.php
@@ -0,0 +1,29 @@
+<div class="loop-meta">
+
+    <?php if ($wrap) echo apply_filters('hcf_edd_open_loop_meta_wrap', '<div class="wrap">'); ?>
+
+    <?php if (is_tax('download_category') || is_tax('download_tag')) { ?>
+
+        <h1 class="loop-title"><?php single_term_title(); ?></h1>
+
+        <div class="loop-description">
+            <?php echo term_description('', get_query_var('taxonomy')); ?>
+            <?php if (function_exists('hcf_edd_download_menu')) hcf_edd_download_menu(); ?>
+        </div><!-- .loop-description -->
+
+    <?php } elseif (is_post_type_archive('download')) { ?>
+
+        <?php $post_type = get_post_type_object(get_query_var('post_type')); ?>
+
+        <h1 class="loop-title"><?php post_type_archive_title(); ?></h1>
+
+        <div class="loop-description">
+            <?php if (!empty($post_type->description)) echo wpautop($post_type->description); ?>
+            <?php if (function_exists('hcf_edd_download_menu')) hcf_edd_download_menu(); ?>
+        </div><!-- .loop-description -->
+
+    <?php } ?>
+
+    <?php if ($wrap) echo apply_filters('hcf_edd_close_loop_meta_wrap', '</div><!-- .wrap -->'); ?>
+
+</div><!-- .loop-meta -->
